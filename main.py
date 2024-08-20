e8d6abe0d8c78fae0a02d6b97dfbb3280206c6c9
Lucas <lboy107@wgu.edu>
2024-08-19 21:06:51 -0400

    Test

--- main.py
+++ main.py
@@ -7,7 +7,8 @@ from pydantic import BaseModel, Field
 from ml.data import apply_label, process_data
 from ml.model import inference, load_model
 
-# DO NOT MODIFY
+# DO NOT MODIFY 
+# Adding notes so this is changed. Using this to push
 class Data(BaseModel):
     age: int = Field(..., example=37)
     workclass: str = Field(..., example="Private")
